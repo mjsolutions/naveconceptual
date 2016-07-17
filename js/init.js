3d77561a68b783b6d35fde083917325b2caf9b87
martinalanis <martinalanis.dev@gmail.com>
2016-07-17 07:46:52 -0500

    Depuracion de archivos js

--- js/init.js
+++ js/init.js
@@ -127,7 +127,7 @@ jQuery(document).ready(function() {
   $('a.page-scroll').on('click', function(event) {
     var $anchor = $(this);
     $('html, body').stop().animate({
-      scrollTop: $($anchor.attr('href')).offset().top
+      scrollTop: $($anchor.attr('href')).offset().top - 60
     }, 1500, 'easeInOutExpo');
     event.preventDefault();
   });
